0dc46c3827833ed2df59f821be5d4acd61e7a4ba
NaKeRNarolino <narolinonaker@gmail.com>
2024-07-29 19:23:12 +0400

    Changed the method to chunks: Faster, more reliable

--- scripts/terdraw.js
+++ scripts/terdraw.js
@@ -7,25 +7,38 @@ export class Terdraw {
   }
 
   static *gen(location, player, heightmap) {
-    for (let x = 0; x < 511; x++) {
-      for (let y = 0; y < 511; y++) {
+    for (let x = 0; x < 32; x++) {
+      for (let y = 0; y < 32; y++) {
         const locationWithoutHeight = {
-          x: x + location.x,
-          y: location.y,
-          z: y + location.z,
+          x: x * 16 + location.x,
+          y: location.y + 320,
+          z: y * 16 + location.z,
         };
-        const finalLocation = {
-          x: x + location.x,
-          y: heightmap[`{\n  \"x\": ${x},\n  \"y\": ${y}\n}`] + location.y,
-          z: y + location.z,
-        };
-
         player.teleport(locationWithoutHeight);
-        player.dimension.fillBlocks(
-          new mc.BlockVolume(locationWithoutHeight, finalLocation),
-          "minecraft:stone",
-          { ignoreChunkBoundErrors: true }
-        );
+        for (let xf = 0; xf < 16; xf++) {
+          for (let yf = 0; yf < 16; yf++) {
+            const startLocation = {
+              x: x * 16 + xf + location.x,
+              y: location.y,
+              z: y * 16 + yf + location.z,
+            };
+            const finalLocation = {
+              x: x * 16 + xf + location.x,
+              y:
+                heightmap[
+                  `{\n  \"x\": ${x * 16 + xf},\n  \"y\": ${y * 16 + yf}\n}`
+                ] + location.y,
+              z: y * 16 + yf + location.z,
+            };
+
+            player.dimension.fillBlocks(
+              new mc.BlockVolume(startLocation, finalLocation),
+              "minecraft:stone",
+              { ignoreChunkBoundErrors: true }
+            );
+          }
+        }
+
         yield;
       }
     }
